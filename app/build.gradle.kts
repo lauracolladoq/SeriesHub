5dd5241e81d5ffd63d903706d551e0c7f281117a
lauracolladoq <laucolqui@gmail.com>
2024-12-02 13:02:47 +0100

    Add login con Google

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,13 +41,15 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
     implementation(libs.firebase.auth)
+    implementation("com.google.android.gms:play-services-auth:21.2.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
