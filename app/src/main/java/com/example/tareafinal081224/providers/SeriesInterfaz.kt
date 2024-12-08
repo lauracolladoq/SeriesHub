c7dd6a4ba043ce0b9e5e5c4a3a6944f2b091b1ac
lauracolladoq <laucolqui@gmail.com>
2024-12-08 21:15:25 +0100

    Final commit

--- app/src/main/java/com/example/tareafinal081224/providers/SeriesInterfaz.kt
+++ app/src/main/java/com/example/tareafinal081224/providers/SeriesInterfaz.kt
@@ -1,10 +1,8 @@
 package com.example.tareafinal081224.providers
 
 import com.example.tareafinal081224.models.ListadoSeries
-import com.example.tareafinal081224.models.Serie
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface SeriesInterfaz {
@@ -16,8 +14,4 @@ interface SeriesInterfaz {
 
     @GET("3/tv/airing_today")
     suspend fun getSeriesAiringToday(@Query("api_key") key: String): Response<ListadoSeries>
-
-    // Obtener una serie por su id usando path ya que query es para parametros de la url dinamica
-    @GET("3/tv/{id}")
-    suspend fun getSerieById(@Path("id") id: Int, @Query("api_key") key: String): Serie
 }
